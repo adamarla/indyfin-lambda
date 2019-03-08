63bae38b2224e4d85d5e19dc95fe3911d5d740d1
adamarla <akshay.damarla@gmail.com>
2019-03-08 10:42:53 -0600

    confirm subscription

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -13,30 +13,66 @@ class CustomJsonEncoder(json.JSONEncoder):
         return super(CustomJsonEncoder, self).default(obj)
 
 
-def generate_checkout_new_url(query_params):
+def generate_checkout_new_url(plan_id):
+    customer_id = "janedoe@jdmail.com"  # TODO: Get user email (or Id) from database table
     result = chargebee.HostedPage.checkout_new({
         "subscription": {
-            "plan_id": query_params.get("plan_id")
+            "plan_id": plan_id
         },
         "customer": {
-            "id": "johndoe@jdmail.com",
-            "first_name": "John",
+            "id": customer_id,
+            "first_name": "Jane",
             "last_name": "Doe",
-            "email": "johnhdoe@jdmail.com"
+            "email": "jd@jdmail.com"
         }
     })
     hosted_page = result._response['hosted_page']
     return json.dumps(hosted_page, cls=CustomJsonEncoder)
 
 
+def confirm_subscription_details(hoste_pae_id):
+    result = chargebee.HostedPage.retrieve(hoste_pae_id)
+    content = result.hosted_page.content
+    subscription = content.subscription
+    customer = content.customer
+    return_obj = {
+        "subscriptionId": subscription.id,
+        "subscriptionPlan": subscription.plan_id,
+        "customerId": customer.id
+    }
+    # TODO - 1. Link subscription ID and plan_id with the customer - this is important
+    # TODO - 2. Based on if link is present, display subscription details
+    #           to customer when they sign in (see get_subscription_details method)
+    #           You can make this call when you are loading customer details, I
+    #           have just implemented it here for example purposes
+    return json.dumps(return_obj, cls=CustomJsonEncoder)
+
+
+def get_subscription_details(subscription_id):
+    result = chargebee.Subscription.retrieve(subscription_id)
+    return {
+        "plan_id": result.subscription.plan_id,
+        "plan_status": result.subscription.status,
+        "plan_start": result.subscription.trial_start,
+        "plan_end": result.subscription.trial_end
+    }
+
+
 def lambda_handler(event, context):
     query_params = event["queryStringParameters"]
+    if "plan_id" in query_params:
+        body = generate_checkout_new_url(query_params["plan_id"])
+    elif "hosted_page_id" in query_params:
+        body = confirm_subscription_details(query_params["hosted_page_id"])
+    else:
+        body = get_subscription_details(query_params["hosted_page_id"])
+
     return {
         'statusCode': 200,
         'headers': {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*"
         },
-        'body': generate_checkout_new_url(query_params)
+        'body': body
     }
 
